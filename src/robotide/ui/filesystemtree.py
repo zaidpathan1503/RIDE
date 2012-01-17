1fbe2706a74139567bafe8ef9f7e04ed8fd9d4f4
Mikko Korpela <mikko.korpela@reaktor.fi>
2012-01-17 21:51:39 +0200

    Add filesystemtree.py

---
+++ src/robotide/ui/filesystemtree.py
@@ -0,0 +1,47 @@
+#  Copyright 2008-2012 Nokia Siemens Networks Oyj
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import wx
+
+try:
+    import wx.lib.agw.customtreectrl as customtreectrl
+except ImportError:
+    import wx.lib.customtreectrl as customtreectrl
+
+
+class FileSystemTree(customtreectrl.CustomTreeCtrl):
+
+    def __init__(self, parent):
+        customtreectrl.CustomTreeCtrl.__init__(self, parent=parent, name=self.__class__.__name__)
+
+
+if __name__ == '__main__':
+    class MyFrame(wx.Frame):
+        def __init__(self, parent, id, title):
+            wx.Frame.__init__(self, parent, id, title)
+    class MyMenuApp(wx.App):
+        def OnInit(self):
+            frame = MyFrame(None , -1, 'Frame Window Demo')
+            sz = wx.BoxSizer()
+            tree2 = FileSystemTree(frame)
+            tree2.AddRoot('ROOT')
+            sz.Add(tree2, 0, wx.GROW|wx.ALL, 5)
+            frame.Show(True)
+            self.SetTopWindow(frame)
+            return True
+    # Run program
+    app=MyMenuApp(0)
+    app.MainLoop()
+
+
+
